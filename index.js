0ab3daf5d0cb620846de1a41d3dc136eb2227f63
Elizabeth <mwendemuthusi6@gmail.com>
2022-11-26 15:08:18 +0300

    Assignment not completed. To unblock

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = "Wewe";
+var bestCustomer;
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+    return customerName;
+    }
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+ console.log(bestCustomer);
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "You";
+    return leastFavoriteCustomer;
+}
+// console.log(customerName)
+// console.log(upperCaseCustomerName())
+// console.log(bestCustomer);
+// console.log(setBestCustomer());
+// console.log(overwriteBestCustomer() );
+console.log(changeLeastFavoriteCustomer());
